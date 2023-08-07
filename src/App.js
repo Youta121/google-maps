253b753cc53cb5bf15ecb651d29bc760bc3a87d7
eya <eya100tounsi@gmail.com>
2023-08-07 19:54:53 +0100

    maps checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const center = [36.7173, 10.2881]; // Tunis coordinates
+const zoom = 13;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={center} zoom={zoom} style={{ width: '100%', height: '100vh' }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={center}>
+        <Popup>Tunis</Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
 export default App;
+
